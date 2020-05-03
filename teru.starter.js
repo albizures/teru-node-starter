27b5b65bf292ccfc47ff3cc2224d72704c6d996f
Jose Albizures <albizures3601@gmail.com>
2020-05-03 20:16:46 +0300

    feat(tokens): migration to ejs

--- teru.starter.js
+++ teru.starter.js
@@ -1,7 +1,19 @@
 module.exports = {
-  tokens: {
-    license_year: {
-      defaultValue: new Date().getFullYear(),
-    },
-  },
+	tokens: {
+		license_year: {
+			title: 'License Year',
+			defaultValue: new Date().getFullYear(),
+		},
+		prettier: {
+			type: 'boolean',
+			defaultValue: false,
+		},
+	},
+	files: {
+		'./prettier.config.js': {
+			onlyWhen: {
+				prettier: true,
+			},
+		},
+	},
 };
